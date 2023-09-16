a488d647944d2594d454d1a63334a2355709106c
Ivan Leschinsky <vanopiano@gmail.com>
2023-09-16 16:42:59 +0300

    Add pve8 script

---
+++ post_install_silent_8.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
+sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/ceph.list
+
+cat <<EOF > /etc/apt/sources.list
+deb http://ftp.debian.org/debian bookworm main contrib
+deb http://ftp.debian.org/debian bookworm-updates main contrib
+deb http://security.debian.org/debian-security bookworm-security main contrib
+deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
+# deb http://download.proxmox.com/debian/pve bookworm pvetest
+EOF
+
+sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+
+systemctl restart pveproxy.service
